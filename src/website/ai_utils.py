3907dc33781e7903aed922af0fbb7d89f5c4b58b
jhonydev <star.jk901@gmail.com>
2022-03-23 22:57:22 +0500

    bugs fixed

--- src/website/ai_utils.py
+++ src/website/ai_utils.py
@@ -23,12 +23,17 @@ def run(file):
     print(file)
     print('LOADED IMAGE PATH OF IMAGE')
     print('----------------------------------------------')
-    json_file = open('fer.json', 'r')
+    from keras.preprocessing import image
+    from keras.models import model_from_json
+    import numpy as np
+    print('LOADED IMAGE PATH OF IMAGE')
+    print('----------------------------------------------')
+    json_file = open('C:\\Users\\Jhony Dev\\PycharmProjects\\cw-ai-expression-detector\\fer.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights("fer.h5")
+    loaded_model.load_weights('C:\\Users\\Jhony Dev\\PycharmProjects\\cw-ai-expression-detector\\fer.h5')
     print("Loaded model from disk")
 
     WIDTH = 48
@@ -37,22 +42,13 @@ def run(file):
     y = None
     labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-    full_size_image = cv2.imread(file)
+    # full_size_image = cv2.imread('photo.jpg', target_size=(WIDTH, HEIGHT), grayscale=True)
+    full_size_image = image.load_img(file, target_size=(WIDTH, HEIGHT), grayscale=True)
     print("Image Loaded")
-    gray = cv2.cvtColor(full_size_image, cv2.COLOR_RGB2GRAY)
-    face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face.detectMultiScale(gray, 1.3, 10)
-    print("FEEDING IMAGE TO MODEL  ")
-    # detecting faces
-    for (x, y, w, h) in faces:
-        print("IMAGES LOOP")
-        roi_gray = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
-        cv2.rectangle(full_size_image, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        #         predicting the emotion
-        yhat = loaded_model.predict(cropped_img)
-        cv2.putText(full_size_image, labels[int(np.argmax(yhat))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0),
-                    1, cv2.LINE_AA)
-        print("Emotion: " + labels[int(np.argmax(yhat))])
-        return labels[int(np.argmax(yhat))]
+    full_size_image = image.img_to_array(full_size_image)
+    full_size_image = np.expand_dims(full_size_image, axis=0)
+    result = loaded_model.predict(full_size_image)
+    # print(result)
+    print(np.argmax(result))
+    print("Emotion: " + labels[int(np.argmax(result))])
+    return labels[int(np.argmax(result))]
